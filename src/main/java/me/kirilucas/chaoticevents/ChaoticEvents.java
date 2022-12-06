5fc1695115ad1a627b895f17285f8b09c8dfe6d4
Lucas Araujo <unconfigured@null.spigotmc.org>
2022-12-06 16:30:25 -0300

    feat: initial commit

---
+++ src/main/java/me/kirilucas/chaoticevents/ChaoticEvents.java
@@ -0,0 +1,12 @@
+package me.kirilucas.chaoticevents;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChaoticEvents extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Chaotic Events is enabled");
+    }
+}
